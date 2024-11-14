26808d803bb6269e9d8d9af2aa837e8a36d4ceae
Expos07 <soneso677@gmail.com>
2024-11-14 15:25:48 +0900

    soon
    LoL

---
+++ Type Conversion.py
@@ -0,0 +1,9 @@
+First = float(input("First: "))
+Second = float(input("Second: "))
+Sum = First + Second
+print('Sum: ' + str(Sum))
+
+int() # 정수 1, 2
+float() # 소수 1.1 1.2
+str() # 문자열
+bool() # True or False
